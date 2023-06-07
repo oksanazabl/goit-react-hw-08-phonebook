9a7fe1aa55b64211d144d8cc9532d204f47cde7a
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-06-07 14:48:19 +0300

    1

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,30 +4,33 @@ import { selectIsLoggedIn } from '../../redux/auth/selectors';
 import UserMenu from '../UserMenu';
 import css from './Navigation.module.css';
 
-
 const Navigation = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
 
-    return (
-      <>< nav>
-      <NavLink className={css.link} to="/">
-        Home
-      </NavLink>
-      {isLoggedIn && (
-        <NavLink className={css.link} to="/contacts">
-          Contacts
+  return (
+    <>
+      <nav>
+        <NavLink className={css.link} to="/">
+          Home
         </NavLink>
-            )}
+        {isLoggedIn ? (
+          <NavLink className={css.link} to="/contacts">
+            Contacts
+          </NavLink>
+        ) : (
+          <div>
+            {' '}
             <NavLink className={css.link} to="/register">
-        Register
+              Register
             </NavLink>
             <NavLink className={css.link} to="/login">
-        Log In
-      </NavLink>
-          
-      </nav >
-       {isLoggedIn && <UserMenu />}</>
-    
+              Log In
+            </NavLink>
+          </div>
+        )}
+      </nav>
+      {isLoggedIn && <UserMenu />}
+    </>
   );
 };
 
